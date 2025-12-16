aa06c3cf0d6842e2fd984bbb4386e0dfe5bf7245
Basi6788 <chick786556@gmail.com>
2025-12-17 01:33:12 +0500

    Refactor IntroActivity to Java from Kotlin

--- app/src/main/java/com/basitshop/IntroActivity.kt
+++ app/src/main/java/com/basitshop/IntroActivity.kt
@@ -1,31 +1,60 @@
-package com.basitshop
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+public class IntroActivity extends AppCompatActivity {
 
-class IntroActivity : AppCompatActivity() {
+    ImageView shapeTop, shapeBottom, iconArrow;
+    View containerMain;
+    LinearLayout layoutButton;
+    FrameLayout btnGlassAction;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
 
-        // Views ko ID se link karna
-        val topDesign = findViewById<ImageView>(R.id.img_top_design)
-        val bottomDesign = findViewById<ImageView>(R.id.img_bottom_design)
-        val brandName = findViewById<TextView>(R.id.txt_brand_name)
+        // Hooks
+        shapeTop = findViewById(R.id.shape_top);
+        shapeBottom = findViewById(R.id.shape_bottom);
+        containerMain = findViewById(R.id.container_main);
+        layoutButton = findViewById(R.id.layout_button);
+        btnGlassAction = findViewById(R.id.btn_glass_action);
+        iconArrow = findViewById(R.id.icon_arrow);
 
-        // Animations Load karna (Make sure anim folder me files hain)
-        val animTop = AnimationUtils.loadAnimation(this, R.anim.top_down)
-        val animBottom = AnimationUtils.loadAnimation(this, R.anim.bottom_up)
-        val animText = AnimationUtils.loadAnimation(this, R.anim.left_fade_in)
+        // Load Animations
+        Animation animFromTop = AnimationUtils.loadAnimation(this, R.anim.from_top);
+        Animation animFromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom);
+        Animation animLeftPop = AnimationUtils.loadAnimation(this, R.anim.left_pop_fade);
+        Animation animButtonBounce = AnimationUtils.loadAnimation(this, R.anim.button_up_bounce);
+        Animation animArrowShake = AnimationUtils.loadAnimation(this, R.anim.arrow_shake);
 
-        // Animations Start karna
-        topDesign.startAnimation(animTop)
-        bottomDesign.startAnimation(animBottom)
-        brandName.startAnimation(animText)
+        // Set Animations
+        shapeTop.startAnimation(animFromTop);
+        shapeBottom.startAnimation(animFromBottom);
+        containerMain.startAnimation(animLeftPop);
+        
+        // Button comes from bottom with bounce
+        layoutButton.startAnimation(animButtonBounce);
+        
+        // Arrow keeps shaking/wiggling inside the button
+        iconArrow.startAnimation(animArrowShake);
+
+        // Click Listener to open Login Screen
+        View.OnClickListener startAction = v -> {
+            Intent intent = new Intent(IntroActivity.this, LoginActivity.class); // LoginActivity class name check kar lena
+            startActivity(intent);
+            // Optional: Finish this activity so user can't go back
+            // finish();
+        };
+
+        btnGlassAction.setOnClickListener(startAction);
+        layoutButton.setOnClickListener(startAction); // Text area click karne par bhi kaam karega
     }
 }
-
