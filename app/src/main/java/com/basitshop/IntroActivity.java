565acb1f20a0038e96205969ab794759c7214f7a
Basi6788 <chick786556@gmail.com>
2025-12-17 02:43:28 +0500

    Update IntroActivity.java

--- app/src/main/java/com/basitshop/IntroActivity.java
+++ app/src/main/java/com/basitshop/IntroActivity.java
@@ -8,31 +8,31 @@ import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
 
     // Variables
     ImageView shapeTop, shapeBottom, iconArrow;
-    LinearLayout containerMain, layoutButton;
+    TextView txtBrandName; // Changed from LinearLayout to TextView
+    LinearLayout layoutButton;
     FrameLayout btnGlassAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Make sure layout file name matches exactly
         setContentView(R.layout.activity_intro);
 
         // 1. IDs find kar rahe hain
-        shapeTop = findViewById(R.id.shape_top);       // Ye top_left.png wali image hai
-        shapeBottom = findViewById(R.id.shape_bottom); // Ye bottom_right.png wali image hai
-        containerMain = findViewById(R.id.container_main);
+        shapeTop = findViewById(R.id.shape_top);
+        shapeBottom = findViewById(R.id.shape_bottom);
+        txtBrandName = findViewById(R.id.txt_brand_name); // New ID
         layoutButton = findViewById(R.id.layout_button);
         btnGlassAction = findViewById(R.id.btn_glass_action);
         iconArrow = findViewById(R.id.icon_arrow);
 
         // 2. Animations Load kar rahe hain
-        // Ye tabhi chalengi jab aap upar di gayi 5 files res/anim folder mein dalenge
         Animation animFromTop = AnimationUtils.loadAnimation(this, R.anim.from_top);
         Animation animFromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom);
         Animation animLeftPop = AnimationUtils.loadAnimation(this, R.anim.left_pop_fade);
@@ -40,19 +40,13 @@ public class IntroActivity extends AppCompatActivity {
         Animation animArrowShake = AnimationUtils.loadAnimation(this, R.anim.arrow_shake);
 
         // 3. Animations Start kar rahe hain
-        // Top Left PNG image slide down hogi
         shapeTop.startAnimation(animFromTop);
-        
-        // Bottom Right PNG image slide up hogi
         shapeBottom.startAnimation(animFromBottom);
         
-        // Beech wala text left se aayega
-        containerMain.startAnimation(animLeftPop);
+        // Ab animation seedha BASITSHOP text par lagegi
+        txtBrandName.startAnimation(animLeftPop);
         
-        // Button neeche se bounce karega
         layoutButton.startAnimation(animButtonBounce);
-        
-        // Arrow hilti rahegi
         iconArrow.startAnimation(animArrowShake);
 
         // 4. Click Listener
