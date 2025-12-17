3d4dedae6b02043113091cff729a6eadc3f066f5
Basi6788 <chick786556@gmail.com>
2025-12-17 16:00:10 +0500

    Update Config.kt

--- app/src/main/java/com/basitshop/Config.kt
+++ app/src/main/java/com/basitshop/Config.kt
@@ -1,7 +1,6 @@
 package com.basitshop
 
 object Config {
-<<<<<<< HEAD
 
     // ===== BASE CONFIG =====
     // NOTE: Yahan slash end me mat lagana
@@ -15,13 +14,3 @@ object Config {
     // const val PROFILE_URL = "$BASE_URL/api/profile"
     // const val LOGOUT_URL = "$BASE_URL/api/logout"
 }
-=======
-    // Jab backend badalna ho, bas yahan change karna
-    const val BASE_URL = "https://Romeo-backend.vercel.app"
-    
-    // Login API Endpoint (Example)
-    const val LOGIN_URL = "$BASE_URL/api/auth/login"
-    const val REGISTER_URL = "$BASE_URL/api/auth/register"
-}
-
->>>>>>> 317de5e (update)
